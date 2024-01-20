79189abf64a8ab852a867bdc0a825dfb63899aaf
StealthyPanda <shaikm259@gmail.com>
2024-01-21 01:14:36 +0530

    Minor changes

--- dec.py
+++ dec.py
@@ -1,27 +1,29 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
-from tensorflow import keras
 from codex import dblog, ok, warning, error
 
+
+
+st.set_page_config(
+    layout = 'wide'
+)
 
 def gpustatus():
     from codex import dblog, ok, warning
     gpu = tf.config.list_physical_devices('GPU')
     if gpu:
         ok('Using GPU')
+        st.info("Using GPU")
         dblog('Found these GPU devices:', '\n', gpu)
     else:
+        st.warning("Not using GPU!")
         warning("No GPU found!")
 gpustatus()
 
 model = tf.keras.models.load_model('decoder.keras')
 
-st.set_page_config(
-    layout = 'wide'
-)
 left, _, right = st.columns([2, 1, 7])
 
 left.markdown("### Model Inputs")
